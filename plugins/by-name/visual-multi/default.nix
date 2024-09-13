81d62d2daae2daf65aa230caa608630f12e6199c
elyth <gwen@omg.lol>
2024-09-13 16:32:48 +0200

    wip

--- plugins/by-name/visual-multi/default.nix
+++ plugins/by-name/visual-multi/default.nix
@@ -8,26 +8,6 @@ nixvim.vim-plugin.mkVimPlugin {
   maintainers = [ lib.maintainers.elythh ];
 
   settingsOptions = {
-    default_mappings = mkNullBool true;
-
-    keys = mkOption {
-      default = { };
-      type = with helpers.nixvimTypes; maybeRaw (attrsOf strLua);
-      description = ''
-        visual-multi mappings declaration.
-        See `:h visual-multi` for more information.
-      '';
-      apply =
-        v:
-        # Handle the raw case first
-        if helpers.nixvimTypes.isRawType v then
-          v
-        # When v is an attrs **but not {__raw = ...}**
-        else
-          mapAttrs (_: helpers.mkRaw) v;
-      example = {
-        "<C-n>" = "Find Word";
-      };
-    };
+    default_mappings = helpers.defaultNullOpts.mkBool false;
   };
 }
